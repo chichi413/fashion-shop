abf41af26dee8402a574b98039f1a3d6b41ac2cd
chichi413 <147756955+chichi413@users.noreply.github.com>
2025-12-17 14:33:59 +0000

    Add MongoDB connection function in db.js

---
+++ db.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+async function connectDB() {
+  const uri = process.env.MONGODB_URI;
+  if (!uri) throw new Error("MONGODB_URI not set in .env");
+
+  mongoose.set("strictQuery", true);
+  await mongoose.connect(uri);
+
+  console.log("✅ MongoDB connected:", mongoose.connection.name);
+}
+
+module.exports = connectDB;
